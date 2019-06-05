b771488d814bbfdf74ed1835dc849bcfc2581569
joovo <wohfacm@gmail.com>
2019-06-05 09:28:34 +0800

    完善修复数据功能

--- fix_data.py
+++ fix_data.py
@@ -2,9 +2,10 @@ import pandas as pd
 import output, PL_parse
 import os
 
-fix_filename='Invalid'
+fix_filename = 'Invalid'
 
-def lazy_fix(filename, Int, detector, date, freq, pattern,flag=True):
+
+def lazy_fix(filename, Int, detector, date, freq, pattern, flag=True):
     global fix_filename
     df = pd.read_csv(filename, header=0)
     # print(filename)
@@ -18,7 +19,7 @@ def lazy_fix(filename, Int, detector, date, freq, pattern,flag=True):
         if flag:
             fix_filename = fix(Int, detector, date, freq, pattern)
         # print(fix_filename)
-        if fix_filename[0]=='0':
+        if fix_filename[0] == '0':
             return '0 Fix Fail'
         return 'Invalid {}'.format(fix_filename)
 
@@ -30,7 +31,7 @@ def fix(Int, detector, date, freq, pattern):
         fix_file_path = './output_2/{}/{}/{}-{}-{}.csv'.format(freq, pattern, Int, before_date, detector)
         if not os.path.isfile(fix_file_path):
             PL_parse.lazy_parse(Int, detector, before_date, freq, pattern)
-        if lazy_fix(fix_file_path, Int, detector, before_date, freq, pattern,flag=False)[0] == 'I':
+        if lazy_fix(fix_file_path, Int, detector, before_date, freq, pattern, flag=False)[0] == 'I':
             raise FileNotFoundError
         return fix_file_path
     except FileNotFoundError:
@@ -39,15 +40,30 @@ def fix(Int, detector, date, freq, pattern):
             print(fix_file_path)
             if not os.path.isfile(fix_file_path):
                 PL_parse.lazy_parse(Int, detector, after_date, freq, pattern)
-            if lazy_fix(fix_file_path, Int, detector, after_date, freq, pattern,flag=False)[0] == 'I':
+            if lazy_fix(fix_file_path, Int, detector, after_date, freq, pattern, flag=False)[0] == 'I':
                 raise FileNotFoundError
             return fix_file_path
         except FileNotFoundError:
             try:
+                # file_dir_path = './output_2/{}/{}/'.format(freq, pattern)
+                # for i in os.listdir(file_dir_path):
+                #     fix_file_path=file_dir_path+i
+                #     if '{}-{}'.format(Int, date) in i and '{}-{}-{}'.format(Int, date, detector) not in i:
+                #         return fix_file_path
+                # else:
+                #     return '0 Fix Fail'
+                lane = pd.read_csv('lane.csv', header=0)
+                straight_way_df = lane[
+                    (lane.iloc[:, 3] == '南直') & (lane.iloc[:, 3] == '北直') & (lane.iloc[:, 3] == '西直') & (lane.iloc[:,
+                                                                                                         3] == '东直') & (
+                            lane.iloc[:, 4] != 0)]
+
+                use_df = straight_way_df[straight_way_df[:, 0] == Int]
+                detector_count = use_df[use_df[:, 0] == Int].iloc[0, 4]
                 file_dir_path = './output_2/{}/{}/'.format(freq, pattern)
-                for i in os.listdir(file_dir_path):
-                    fix_file_path=file_dir_path+i
-                    if '{}-{}'.format(Int, date) in i and '{}-{}-{}'.format(Int, date, detector) not in i:
+                for d in detector_count:
+                    fix_file_path = file_dir_path + '{}-{}-{}.csv'.format(Int, date, detector)
+                    if os.path.isfile(fix_file_path):
                         return fix_file_path
                 else:
                     return '0 Fix Fail'
